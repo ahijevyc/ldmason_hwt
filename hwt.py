c7a98a9822d348d01d1864d222b5cab7b5c34287
ahijevyc <ahijevyc@ucar.edu>
2025-03-17 11:28:44 -0600

    imports for xtime in hwt.py

--- hwt.py
+++ hwt.py
@@ -1,3 +1,6 @@
+import logging
+from pathlib import Path
+
 import cartopy
 import pandas as pd
 import xarray
@@ -5,9 +8,9 @@ import xarray
 
 class Model:
     """model has a name and number of members
-    It starts with no variables, v, to analyze, 
-    but when group attribute is assigned, the 
-    v attribute is also assigned. """
+    It starts with no variables, v, to analyze,
+    but when group attribute is assigned, the
+    v attribute is also assigned."""
 
     def __init__(self, name, nmem, lead_time_days):
         self.name = name
@@ -21,16 +24,19 @@ class Model:
         return self.name
 
     """ set the v property when the group property is set """
+
     @property
     def group(self):
         return self._group
 
     @group.setter
-    def group(self, value:str):
+    def group(self, value: str):
         possible_groups = ["wind", "uh"]
-        assert value in possible_groups, f"unexpected group {value}. Expected one of {possible_groups}"
+        assert (
+            value in possible_groups
+        ), f"unexpected group {value}. Expected one of {possible_groups}"
         if self.name == "fv3":
-            if value == "uh": 
+            if value == "uh":
                 v = ["MXUPHL0_1km_max", "MXUPHL0_3km_max", "MXUPHL2_5km_max"]
             else:
                 v = ["MAXUVV_max", "MAXWIND10m"]
@@ -51,25 +57,24 @@ def firstRun(year=2024):
 
 
 # for interp output
-projection =  cartopy.crs.LambertConformal(
+projection = cartopy.crs.LambertConformal(
     central_longitude=262.5 - 360,
-    standard_parallels=(38.5,38.5),
+    standard_parallels=(38.5, 38.5),
 )
 extent = (-123.0, -72.1, 21.6, 50.4)
-
 
 
 # Multiple thresholds for updraft helicity
 helicityThresholds = xarray.DataArray(
     [10, 20, 30, 40, 50, 75, 100, 125, 150, 200, 250, 300],
     dims="thresh",
-    attrs={"units": "$m^2/s^2$", "short_name":"uh"},
+    attrs={"units": "$m^2/s^2$", "short_name": "uh"},
 )
 
 windThresholds = xarray.DataArray(
     [5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
     dims="thresh",
-    attrs={"units": "$m/s$", "short_name":"wind"},
+    attrs={"units": "$m/s$", "short_name": "wind"},
 )
 
 fv3 = Model(
